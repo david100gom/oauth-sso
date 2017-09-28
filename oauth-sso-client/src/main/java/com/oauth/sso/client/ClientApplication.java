a84069c5f0d704862faa050008e52e996c0fc8c2
quickmenu <david100gom@gmail.com>
2017-09-28 18:30:15 +0900

    add SSO Client part 1

--- oauth-sso-client/src/main/java/com/oauth/sso/client/ClientApplication.java
+++ oauth-sso-client/src/main/java/com/oauth/sso/client/ClientApplication.java
@@ -1,10 +1,58 @@
 package com.oauth.sso.client;
 
+import com.oauth.sso.client.domain.User;
+import com.oauth.sso.client.service.UserRepository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Random;
 
 @SpringBootApplication
 public class ClientApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(ClientApplication.class);
+
+	@Value("${systemName}")
+	private String systemName;
+
+	@Value("${server.port}")
+	private int serverPort;
+
+	@Bean
+	public CommandLineRunner initUser(UserRepository repository) {
+		//
+		return (args) -> {
+			//
+			log.debug("\n## systemName : {}", systemName);
+			log.debug("\n## systemPort : {}", serverPort);
+
+			repository.save(createUser("tsong"));
+			repository.save(createUser("jmpark"));
+			repository.save(createUser("jkkang"));
+
+			for (User user : repository.findAll()) {
+				log.debug("\n## found user : {}", user);
+			}
+			User user = repository.findOne("jmpark");
+			log.debug("\n## user : {}", user);
+		};
+	}
+
+	private User createUser(String userName) {
+		//
+		return new User(userName, getRandomAge(), String.format("%s in %s", userName, systemName));
+	}
+
+	private int getRandomAge() {
+		//
+		return 20 + new Random().nextInt(30);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientApplication.class, args);
